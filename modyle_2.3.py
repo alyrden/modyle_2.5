30fa169c8fc8b12feba176e957d6f2677b98eed9
alyrden <f50knox@gmail.com>
2024-08-27 17:30:08 +0300

    2.3

--- modyle_2.3.py
+++ modyle_2.3.py
@@ -1,2 +1,8 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-print(my_list)
+st_ = 0
+while st_ < len(my_list):
+      if my_list[st_] >= 0:
+        print(my_list[st_])
+      st_ += 1
+      if my_list[st_] < 0:
+          break
