3201be1dd54b783884b1d370691c68383a462e3a
alyrden <f50knox@gmail.com>
2024-08-29 10:46:56 +0300

    2.3

--- modyle_2.3.py
+++ modyle_2.3.py
@@ -1,8 +1,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 st_ = 0
 while st_ < len(my_list):
-      if my_list[st_] >= 0:
+    if my_list[st_] > 0:
         print(my_list[st_])
-      st_ += 1
-      if my_list[st_] < 0:
-          break
+        st_ += 1
+        if my_list[st_] == 0:
+            st_ += 1
+            continue
+        else:
+            if my_list[st_] < 0:
+                break
+# Если условие сразу встретит число меньше 0, то закончит выполнение.
