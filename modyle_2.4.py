75f2e7f78054ae4e430eb3029eef0224a2597f9f
alyrden <f50knox@gmail.com>
2024-09-03 17:43:47 +0300

    2.4

--- modyle_2.4.py
+++ modyle_2.4.py
@@ -1,11 +1,11 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes = list()
-not_primes = list()
 n = 2
+primes = []
+not_primes = []
 is_prime = True
 for i in numbers:
     for y in range(len(numbers)):
-        if i % n == 0:
+        if y % n == 0:
             is_prime = False
             print(primes)
         else:
