4a42cb1894df3e2105ac0a8556b90b5cfac76c67
alyrden <f50knox@gmail.com>
2024-09-11 21:36:00 +0300

    2.5

--- modyle_2.4.py
+++ modyle_2.4.py
@@ -1,12 +1,22 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
 primes = []
 not_primes = []
-is_prime = True
-for primes in numbers:
-    n = 0
-    for not_primes in numbers:
-        if primes % not_primes == 0 and primes != 0 and not_primes != 1:
-            n += 1
-            print(primes)
-        else:
-            print(not_primes)
+
+for i in numbers:
+    is_prime = True
+
+    if i < 2:
+        is_prime = False
+    else:
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+
+print("Простые числа:", primes)
+print("Составные числа:", not_primes)
